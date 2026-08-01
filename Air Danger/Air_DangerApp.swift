cd0ec0d3d9bce358631d829dcb42cdddeac80303
Volodymyr Maksymchuk <volodymyr.maksymchuk@gen.tech>
2026-08-01 19:36:43 +0300

    Initial Commit

---
+++ Air Danger/Air_DangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_DangerApp.swift
+//  Air Danger
+//
+//  Created by Volodymyr Maksymchuk on 01.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct Air_DangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
